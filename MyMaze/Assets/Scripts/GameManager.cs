e39a1ac24fc2578840feb659e6e6c4f0ecf4c155
Dying-Zoe <48019332+Dying-Zoe@users.noreply.github.com>
2019-03-05 12:31:46 +0300

    Maze and movement

---
+++ MyMaze/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject playerPrefab;
+
+    private GameObject playerInstance;
+
+    public Maze mazePrefab;
+
+	private Maze mazeInstance;
+
+	private void Start ()
+    {
+        StartCoroutine(BeginGame());
+    }
+	
+	private void Update () {
+		if (Input.GetKeyDown(KeyCode.R)) {
+			RestartGame();
+		}
+	}
+
+    [System.Obsolete]
+    private IEnumerator BeginGame()
+    {
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+        yield return StartCoroutine(mazeInstance.Generate());
+        playerInstance = Instantiate(playerPrefab, mazeInstance.GetCell(new IntVector2(UnityEngine.Random.RandomRange(0,20),UnityEngine.Random.RandomRange(0, 20))).transform.position, Quaternion.identity);
+    }
+
+	private void RestartGame () {
+        StopAllCoroutines();
+        Destroy(mazeInstance.gameObject);
+        if (playerInstance != null)
+        {
+            Destroy(playerInstance.gameObject);
+        }
+        StartCoroutine(BeginGame());
+	}
+}
